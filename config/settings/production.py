6965ab676276b52562da1b2f0d8c157ec186bfa6
Danushree88 <danushree86@gmail.com>
2026-03-20 14:46:37 +0530

    Add deployment config

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,19 @@
 from .base import *
+import os
 
 DEBUG = False
 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Use Railway's provided DATABASE_URL if available
+import dj_database_url
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
